db17e0a1eb67c9162f9754e451b5a0e3481b7066
Choco1125 <sebascruz981125@gmail.com>
2026-07-26 22:01:59 -0500

    feat: create server

---
+++ 02-node-web/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+const SALUDO = process.env.SALUDO || '¡Hola desde Node.js dentro de Docker!'
+
+app.get('/', (req, res)=> {
+	console.log('[GET] /')
+
+	res.json({
+		mensaje: SALUDO,
+		hostname: process.env.HOSTNAME,
+		timestamp: new Date().toISOString(),
+	})
+})
+
+// Endpoint de salud, útil para HEALTHCHECK y orquestadores
+app.get('/health', (req,res) => {
+	console.log('[GET] /health')
+	res.status(200).json({
+		status: 'ok'
+	})
+})
+
+app.listen(PORT, () => {
+	console.log(`Server running on port http://localhost:${PORT}`)
+})
